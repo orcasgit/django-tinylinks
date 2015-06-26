515ef1a5308f44e89f6dcca0aea7342f6254024e
Jess Johnson <jess@grokcode.com>
2015-06-26 14:24:28 -0700

    Add Django 1.7+ style migrations.

--- tinylinks/migrations/0001_initial.py
+++ tinylinks/migrations/0001_initial.py
@@ -1,35 +1,34 @@
-# flake8: noqa
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from django.utils.timezone import utc
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Tinylink'
-        db.create_table('tinylinks_tinylink', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('long_url', self.gf('django.db.models.fields.CharField')(max_length=2500)),
-            ('short_url', self.gf('django.db.models.fields.CharField')(unique=True, max_length=32)),
-        ))
-        db.send_create_signal('tinylinks', ['Tinylink'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Tinylink'
-        db.delete_table('tinylinks_tinylink')
-
-
-    models = {
-        'tinylinks.tinylink': {
-            'Meta': {'object_name': 'Tinylink'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'long_url': ('django.db.models.fields.CharField', [], {'max_length': '2500'}),
-            'short_url': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'})
-        }
-    }
-
-    complete_apps = ['tinylinks']
+    operations = [
+        migrations.CreateModel(
+            name='Tinylink',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('long_url', models.CharField(max_length=2500, verbose_name='Long URL')),
+                ('short_url', models.CharField(unique=True, max_length=32, verbose_name='Short URL')),
+                ('is_broken', models.BooleanField(default=False, verbose_name='Status')),
+                ('validation_error', models.CharField(default=b'', max_length=100, verbose_name='Validation Error')),
+                ('last_checked', models.DateTimeField(default=datetime.datetime(2015, 6, 26, 21, 18, 13, 702405, tzinfo=utc), verbose_name='Last validation')),
+                ('amount_of_views', models.PositiveIntegerField(default=0, verbose_name='Amount of views')),
+                ('redirect_location', models.CharField(default=b'', max_length=2500, verbose_name='Redirect location')),
+                ('user', models.ForeignKey(related_name='tinylinks', verbose_name='Author', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['-id'],
+            },
+        ),
+    ]
